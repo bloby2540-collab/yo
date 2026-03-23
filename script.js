f6c53462286629089a1f3bf7f1fd964642d715e2
bloby2540-collab <bloby2540@gmail.com>
2026-03-23 11:53:00 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+
+function play_slope(){
+var win = window.open('about:blank');
+var url = "https://bigfoot9999.github.io/Slope-Game/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+function play_clash_royale(){
+var win = window.open('about:blank');
+var url = "https://web.cloudmoonapp.com/game/com.supercell.clashroyale/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+function play_drift_boss(){
+var win = window.open('about:blank');
+var url = "https://driftbossonline.com/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+
+function find_skins(){
+var win = window.open('about:blank');
+var url = "https://crafty.gg/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+function fake_update(){
+var win = window.open('about:blank');
+var url = "https://www.whitescreen.online/fake-chrome-os-update-screen/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+
+function basket_random(){
+var win = window.open('about:blank');
+var url = "https://www.twoplayergames.org/gameframe/basket-random#"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+
+function roblox_fun(){
+var win = window.open('about:blank');
+var url = "https://www.roblox.com/home#"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+
+function Rocket_Leauge(){
+var win = window.open('about:blank');
+var url = "https://rocketgoal.io/?utm_source=classroom6x;" // Replace with your game
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
